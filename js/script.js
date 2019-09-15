304d9d74422704ddbacb7af3ae03032535ee1644
Tikondra <tikondra47@gmail.com>
2019-09-15 20:58:15 +0400

    Я закончила главную!!! ...вроде )))

--- js/script.js
+++ js/script.js
@@ -13,36 +13,79 @@ function initMap() {
     var imgpin = 'img/pin.svg'  
     var marker = new google.maps.Marker({
     
-    position: {lat: 59.938495, lng: 30.322907},
-    
-    map: map,    
-    title: 'Gllacy Shop',
-    icon: imgpin
-});
+        position: {lat: 59.938495, lng: 30.322907},
+        
+        map: map,    
+        title: 'Gllacy Shop',
+        icon: imgpin
+    });
 }
 
-var popup = document.querySelector('.modal-feedback');
-var openPopupButton = document.querySelector('.map-contacts__link');
-var closePopupButton = popup.querySelector('.feedback-form__btn_close');
-var overlay = document.querySelector('body');
+function feedback() {
 
-openPopupButton.addEventListener('click', function (evt) {
+    var popupFeedback = document.querySelector('.modal-feedback');
+    var openPopupButton = document.querySelector('.map-contacts__link');
+    var closePopupButton = popupFeedback.querySelector('.feedback-form__btn_close');
+    var overlay = document.querySelector('body');
 
-    evt.preventDefault();
-    popup.classList.add('modal__show');
-    overlay.classList.add('overlay');
-});
+    openPopupButton.addEventListener('click', function (evt) {
 
-closePopupButton.addEventListener('click', function () {
-    popup.classList.remove('modal__show');
-    overlay.classList.remove('overlay');
-});
+        evt.preventDefault();
+        popupFeedback.classList.add('modal__show');
+        overlay.classList.add('overlay');
+    });
 
-document.addEventListener('keydown', function (evt) {
-
-    if (evt.keyCode === 27) {
-        popup.classList.remove('modal__show');
+    closePopupButton.addEventListener('click', function () {
+        popupFeedback.classList.remove('modal__show');
         overlay.classList.remove('overlay');
-    }    
-});
+    });
 
+    document.addEventListener('keydown', function (evt) {
+
+        if (evt.keyCode === 27) {
+            popupFeedback.classList.remove('modal__show');
+            overlay.classList.remove('overlay');
+        }    
+    });
+};
+
+function search() {
+    var popupSearch = document.querySelector('.search-form');
+    var openPopupButton = document.querySelector('.header-auth__link_search');
+
+    openPopupButton.addEventListener('click', function (evt) {
+
+        evt.preventDefault();
+        popupSearch.classList.add('search-form_show');        
+    });
+
+    document.addEventListener('keydown', function (evt) {
+
+        if (evt.keyCode === 27) {
+            popupSearch.classList.remove('search-form_show');            
+        }    
+    });
+}
+
+function enter() {
+    var popup = document.querySelector('.enter-form');
+    var openPopupButton = document.querySelector('.header-auth__link_enter');
+
+    openPopupButton.addEventListener('click', function (evt) {
+
+        evt.preventDefault();
+        popup.classList.add('enter-form_show');        
+    });
+
+    document.addEventListener('keydown', function (evt) {
+
+        if (evt.keyCode === 27) {
+            popup.classList.remove('enter-form_show');            
+        }    
+    });
+}
+
+
+feedback();
+search();
+enter();
